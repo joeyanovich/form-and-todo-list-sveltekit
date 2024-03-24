52d36501b3e1ee3d6d34a5d6b6484df4ab638c6b
joeyanovich <joedison.dias@gmail.com>
2024-03-23 23:25:20 -0300

    font ✏️: poppins font import to project

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
+    fontFamily: {
+      body: ['Poppins', 'sans-serif'],
+      // title: ['Anton', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
