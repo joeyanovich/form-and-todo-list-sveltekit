1b77700a226da0fcdec10d2a285e9f54d2b7c2e6
joeyanovich <joedison.dias@gmail.com>
2024-03-25 00:15:31 -0300

    work 🚧: style and structure of dashboard page

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,1 +1,12 @@
-asd
+<div class="flex flex-col justify-between h-screen p-6 m-auto w-full max-w-5xl">
+  <header class="flex item-center justify-between">
+    <h1 class="uppercase font-black text-3xl">Todo List</h1>
+    <button class="px-7 py-3 rounded-lg text-white hover:bg-transparent hover:text-slate-600 bg-slate-600 ease-in-out duration-500">Sair</button>
+  </header>
+  <main class="flex flex-col gap-2 border-2 rounded-3xl border-gray-500">
+    <div class="flex items-stretch">
+      <input class="flex-1 rounded-l-3xl border-none p-4 focus:outline-none text-slate-500 bg-transparent" type="text" placeholder="Digite sua tarefa">
+      <button class="px-4 border-none rounded-r-2xl text-white bg-slate-600 ease-in-out duration-500 hover:bg-transparent hover:text-slate-600">Adicionar</button>
+    </div>
+  </main>
+</div>
